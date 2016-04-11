9d7509a6924bd65a8c6c98485ef563d7d201a81e
Michael G Davis <mggdavis@gmail.com>
2016-04-11 20:15:51 +0000

    Added debug to many layers including up to form partial, and edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
     def new
         # form where a user can fill out their own profile.
         @user = User.find( params[:user_id])
-        @profile = @user.build.profile
+        @profile = Profile.new
     end
     
     def create
@@ -15,6 +15,11 @@ class ProfilesController < ApplicationController
         else
             render action: :new
         end
+    end
+    
+    def edit
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
     end
     
     private
