fd0af00fe6d3ac68211c9ef64bc845d79e0d2a2f
Michael G Davis <mggdavis@gmail.com>
2016-04-11 19:53:35 +0000

    Added debug to prfiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,24 @@
 class ProfilesController < ApplicationController
-   
-end
+
+    def new
+        # form where a user can fill out their own profile.
+        @user = User.find( params[:user_id])
+        @profile = @user.build.profile
+    end
+    
+    def create
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:succes] = "Profile Update!"
+            redirect_to user_path( params[:user_id] )
+        else
+            render action: :new
+        end
+    end
+    
+    private
+        def profile_params
+            params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
+end            
